da3203d0169f1c7fdebed2f7f0b41314e8aec3ad
Dennis Rodrigo <62552509+Darispeg@users.noreply.github.com>
2023-06-24 03:11:29 -0400

    "open 'api/v1/files' route"

--- src/main/java/com/example/api_rest/security/WebSecurityConfig.java
+++ src/main/java/com/example/api_rest/security/WebSecurityConfig.java
@@ -36,7 +36,6 @@ public class WebSecurityConfig {
                 .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v1/files/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
